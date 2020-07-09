05a294329f32cf918ff05388f3e15bdce58fa0a5
Anna <ikokianna@gmail.com>
2020-07-09 16:02:04 +0200

    added UI Relates #2

--- scripts/script-variables.js
+++ scripts/script-variables.js
@@ -1,1 +1,7 @@
+console.log('--- loading: global variables');
+
+const menu = `Click an operation, you'll be prompted to enter a number.
+  You will be prompted until the number is valid.
+  To cancel the operation, click "cancel".`;
+
 let numbers = [];
