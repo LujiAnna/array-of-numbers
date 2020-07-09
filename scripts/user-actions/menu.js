4415249e807487ed57ed21db3f0a50612102bdb0
Anna <ikokianna@gmail.com>
2020-07-09 15:49:33 +0200

    added html-css files and menu Relates #é

---
+++ scripts/user-actions/menu.js
@@ -0,0 +1,16 @@
+function menuHandler() {
+console.log('--user action: menu--');
+console.log('menu: ', typeof menu, '\n', menu);
+
+alert(menu);
+}
+
+
+
+// function instructionsHandler() {
+//     console.log('-- action: instructions --');
+//     console.log('instructions:', typeof instructions, '\n', instructions);
+//     alert(instructions);
+//   };
+
+
