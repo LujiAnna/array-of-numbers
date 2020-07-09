2bdbbac3796c09b6215f7e3ab8eca17da1fff38a
harunaltunhr <altunhr@gmail.com>
2020-07-09 08:49:15 +0200

    Add-number-js completed
    Closes #5

--- scripts/user-actions/add-number.js
+++ scripts/user-actions/add-number.js
@@ -4,22 +4,22 @@ console.log('--- loading: add number');
 const addNumberHandler = () => {
   console.log('\n--- calling: add number handler');
 
-  const userInput = _;
+  const userInput = Number(prompt('Please add a number:'))
   console.log('userInput:', typeof userInput, '\n', userInput);
-  if (_) {
+  if (typeof(Number(userInput))!=='number') {
     return;
   }
 
-  const userNumber = _;
+  const userNumber =Number(userInput);
   console.log('userNumber:', typeof userNumber, '\n', userNumber);
-  if (_) {
+  if (typeof(userNumber)!=='number') {
     alert(`${userInput} is not a valid number`);
     return;
   }
 
   console.log('numbers (before):', typeof numbers, '\n', numbers);
   // add the user's number to the main shared array of numbers
-  _;
+  const arrOfNumbers= numbers.push(userInput);
   console.log('numbers (after):', typeof numbers, '\n', numbers);
 
   displayArrayHandler();
